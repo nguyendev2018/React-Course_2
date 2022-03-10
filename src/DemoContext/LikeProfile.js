2986da380cbc3e76e6d26c771f883c6f611e108e
nguyendev2018 <nguyendev2018@gmail.con>
2022-03-10 16:28:46 +0700

    --- Auto-Git Commit ---

--- src/DemoContext/LikeProfile.js
+++ src/DemoContext/LikeProfile.js
@@ -10,7 +10,7 @@ export default class LikeProfile extends Component {
                     return <div>
                         <div className="card text-dark bg-default " style={{ width: 300 }}>
                             <div className="card-body">
-                                <button style={{ color: "red", border: "1px solid red" }} className="card-title">Like</button>
+                                <button onClick={() => { value.setState() }} style={{ color: "red", border: "1px solid red" }} className="card-title">Like </button>
                             </div>
                         </div>
                     </div>
